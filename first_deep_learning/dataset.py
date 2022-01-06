6977b89de69ab5ad6f8e4c390646986872797d9e
S-Nakamur-a <25317025+S-Nakamur-a@users.noreply.github.com>
2022-01-06 14:33:24 +0900

    first commit

---
+++ first_deep_learning/dataset.py
@@ -0,0 +1,77 @@
+import cv2
+import tqdm
+from typing import Callable
+import numpy as np
+import albumentations as alb
+import random
+from pathlib import Path
+
+
+MARK_COLOR = 255
+
+# create mahjong tile
+# mark_function should receive and return np.ndarray (range: [0, 255], shape: (height, width), dtype: np.uint8))
+def create_mahjong_tile(height: int, width: int, mark_function: Callable[[np.ndarray], np.ndarray]) -> np.ndarray:
+    tile = np.zeros((height, width), dtype=np.uint8)
+    return mark_function(tile)
+
+def draw_bamboo(tile: np.ndarray, height: int, width: int, x: int, y: int):
+    tile[y:y+height, x] = MARK_COLOR
+    tile[y, x-width//2:x+width//2+1] = MARK_COLOR
+    tile[y+height//2, x-width//2:x+width//2+1] = MARK_COLOR
+    tile[y+height, x-width//2:x+width//2+1] = MARK_COLOR
+
+
+def two_bamboos(tile: np.ndarray) -> np.ndarray:
+    h, w = tile.shape
+    margin = h // 10
+    bamboo_height = (h - 3 * margin) // 2
+    bamboo_width = w // 8
+    draw_bamboo(tile, bamboo_height, bamboo_width, w//2, margin)
+    draw_bamboo(tile, bamboo_height, bamboo_width, w//2, h-margin-bamboo_height)
+    return tile
+
+def three_bamboos(tile: np.ndarray) -> np.ndarray:
+    h, w = tile.shape
+    margin = max(h // 10, 1)
+    bamboo_height = (h - 3 * margin) // 2
+    bamboo_width = max(w // 8, 2)
+    draw_bamboo(tile, bamboo_height, bamboo_width, w//2, margin)
+    draw_bamboo(tile, bamboo_height, bamboo_width, w//4, h-margin-bamboo_height)
+    draw_bamboo(tile, bamboo_height, bamboo_width, w//4 * 3, h-margin-bamboo_height)
+    return tile
+
+def four_bamboos(tile: np.ndarray) -> np.ndarray:
+    h, w = tile.shape
+    margin = h // 10
+    bamboo_height = (h - 3 * margin) // 2
+    bamboo_width = w // 8
+    draw_bamboo(tile, bamboo_height, bamboo_width, w//4, margin)
+    draw_bamboo(tile, bamboo_height, bamboo_width, w//4, h-margin-bamboo_height)
+    draw_bamboo(tile, bamboo_height, bamboo_width, 3 * w//4, margin)
+    draw_bamboo(tile, bamboo_height, bamboo_width, 3 * w//4, h-margin-bamboo_height)
+    return tile
+
+def randomize_tile(tile: np.ndarray) -> np.ndarray:
+    compose = alb.Compose(
+        [
+            alb.Rotate(always_apply=True, limit=5),
+        ]
+    )
+    return compose(image=tile)["image"]
+
+def create_random_tiles(n_tiles: int, size: int, out_dir: Path):
+    functions = [two_bamboos, three_bamboos, four_bamboos]
+    names = ["2_bamboos", "3_bamboos", "4_bamboos"]
+    height = size - 4
+    width = height // 2
+    for n in tqdm.tqdm(range(n_tiles * len(functions))):
+        f_i = n // n_tiles
+        tile = randomize_tile(create_mahjong_tile(height, width, functions[f_i]))
+        image = np.zeros((size, size), dtype=np.uint8)
+        image[(size-height)//2:(size-height)//2+height, (size-width)//2:(size-width)//2+width] = tile
+        cv2.imwrite(str(out_dir / f"{names[f_i]}_{n:04}.png"), image)
+
+
+if __name__ == "__main__":
+    create_random_tiles(15, 40, Path(__file__).parents[1] / "mahjong_tiles")
---
+++ first_deep_learning/feature_extractor.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+
+def convolution(image: np.ndarray, kernel: np.ndarray, stride: int, pad: int):
+    h, w = image.shape
+    t = np.zeros((h + 2 * pad,  w + 2 * pad), dtype=np.uint8)
+    image = t[pad:pad+h, pad:pad+w] = image
+    h, w = image.shape
+
+    k_h, k_w = kernel.shape
+
+    out_h = (h - k_h) // stride
+    out_w = (w - k_w) // stride
+    out = np.zeros((out_h, out_w), dtype=np.uint8)
+
+    for y in range(out_h):
+        for x in range(out_w):
+            v = np.sum(image[y*stride:y*stride+k_h, x*stride:x*stride+k_w] * kernel)
+            out[y][x] = v
+    
+    return out.astype(np.uint8)
+
+
+
+# def emphasize_bar(image: np.ndarray):
+#     kernel = np.array([
+#         [0, 0, 1, 0, 0],
+#         [0, 0, 1, 0, 0],
+#         [0, 0, 1, 0, 0],
+#         [0, 0, 1, 0, 0],
+#         [0, 0, 1, 0, 0],
+#     ],
+#     dtype=np.float16
+#     ) / 5
+#     return convolution(image, kernel, stride=1, pad=2)
+
+
+def emphasize_bar(image: np.ndarray):
+    kernel = np.array([
+        [-1, -1, 0, 1, 1],
+        [-1, -1, 0, 1, 1],
+        [-1, -1, 0, 1, 1],
+        [-1, -1, 0, 1, 1],
+        [-1, -1, 0, 1, 1],
+    ],
+    dtype=np.float16
+    )
+    return convolution(image, kernel, stride=2, pad=2)
+
+
+def average(image: np.ndarray):
+    size = 9
+    kernel = np.ones((size, size), dtype=np.float16) / (size * size)
+    return convolution(image, kernel, stride=3, pad=3)
+
+
+if __name__ == '__main__':
+    import cv2
+    from pathlib import Path
+    import tqdm
+
+    out = Path(__file__).parents[1] / "features"
+
+    for image_path in tqdm.tqdm((Path(__file__).parents[1] / "mahjong_tiles").glob("*.png")):
+        image = cv2.imread(str(image_path), cv2.IMREAD_GRAYSCALE)
+        image = 2 * average(emphasize_bar(image))
+        cv2.imwrite(str(out / image_path.name), image)
